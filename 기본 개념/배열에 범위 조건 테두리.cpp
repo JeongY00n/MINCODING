982a7cd8324f5ae2ab1da5f01b2d4dfdcd7f99f8
JeongY00n <shimmumu@gmail.com>
2022-08-06 19:10:32 +0900

    Create 배열에 범위 조건 테두리.cpp

---
+++ 기본 개념/배열에 범위 조건 테두리.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    ///////////////////////////////////////////////////////////////
+    //배열의 범위를 벗어난 경우 제외시키는 조건을 
+    //지정 값을 제외한 나머지에 대해서 -1의 울타리를 채워준다
+    //ex) 
+    /*
+    
+    int map[1010][1010] = {0};
+
+    for (int i = 0; i < 1010; i++) 
+        for (int j = 0; j < 1010; j++) 
+            map[i][j] = -1;
+        
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> map[i][j];        }
+    }
+
+    
+
+
+    if(map[nextRow][nextCol]==-1) continue;
+    */
+
+
+
+    return 0;
+}
+
+
