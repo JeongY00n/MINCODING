8f95ddd649f76d107f0d527d8c0a8c0503a1d803
JeongY00n <shimmumu@gmail.com>
2022-08-08 23:28:38 +0900

    Update 36-4.포스터 붙이기.cpp

--- Level36_Dijkstra와 Floyd/36-4.포스터 붙이기.cpp
+++ Level36_Dijkstra와 Floyd/36-4.포스터 붙이기.cpp
@@ -8,53 +8,66 @@ struct Edge {
     int cost;
 };
 
-struct cmdstruct {
-    bool operator()(Edge A, Edge B) {
-        if (A.cost < B.cost)return false;
-        if (A.cost > B.cost)return true;
-
-        if (A.to < B.to)return false;
-        if (A.to > B.to)return true;
+struct cmpstruct {
+    bool operator()(Edge a, Edge b) {
+        if (a.cost < b.cost)return false;
+        if (a.cost > b.cost)return true;
 
         return false;
     }
-
 };
-
-
-int C, P, K, A, B;
+int edge, node, K, A, B;
+priority_queue<Edge, vector<Edge>, cmpstruct> pq;
 vector<Edge> v[100001];
-priority_queue<Edge, vector<Edge>, cmdstruct> pq;
+int result = 0;
+int dijkstra(int k) {
+    int used[100001] = { 0 };
+    int dist[100001] = { 0 };
 
-void dijkstra(int start, int* dist)
-{
-    pq.push({ start, 0 });
-
-    for (int i = 1; i <= P; i++)
+    for (int i = 1; i <= node; i++)
         dist[i] = 2134567890;
 
-    dist[start] = 0;
-
-    int used[100001] = { 0 };
+    pq.push({ k,0 });
+    dist[k] = 0;
     
     while (!pq.empty()) {
-
-        Edge now = pq.top(); pq.pop();
+        Edge now = pq.top();
+        pq.pop();
 
         if (used[now.to] == 1)continue;
-        used[now.to] = 1;
 
+        used[now.to] = 1;
 
         for (int i = 0; i < v[now.to].size(); i++) {
             Edge next = v[now.to][i];
 
-            if (dist[next.to] <= dist[now.to] + next.cost)continue;
+            if (dist[next.to] < dist[now.to] + next.cost) continue;
             dist[next.to] = dist[now.to] + next.cost;
-
-            pq.push({ next.to, next.cost });
+            pq.push({ next.to, dist[next.to] });
         }
     }
-
+    if (k == K) {
+        if (dist[A] < dist[B])
+        {
+            result += dist[A];
+            return A;
+        }
+        else
+        {
+            result += dist[B];
+            return B;
+        }
+    }
+    else if (k == A)
+    {
+        result += dist[B];
+        return result;
+    }
+    else if (k == B)
+    {
+        result += dist[A];
+        return result;
+    }
 }
 
 int main()
@@ -63,22 +76,19 @@ int main()
     cin.tie();
     cout.tie();
 
-    cin >> C >> P >> K >> A >> B;
+    cin >> edge >> node >> K >> A >> B;
 
-    for (int i = 0; i < C; i++) {
-        int p1, p2, d;
-        cin >> p1 >> p2 >> d;
-        v[p1].push_back({ p2,d });
-        v[p2].push_back({ p1,d });
+    for (int i = 0; i < edge; i++) {
+        int from, to, cost;
+        cin >> from >> to >> cost;
+        v[from].push_back({ to,cost });
+        v[to].push_back({ from,cost });
     }
 
-    int dist1[100001];
-    dijkstra(K, dist1);
-    int dist2[100001];
-    dijkstra(A, dist2);
+    int next = dijkstra(K);
+    result = dijkstra(next);
 
+    cout << result;
 
     return 0;
 }
-
-
