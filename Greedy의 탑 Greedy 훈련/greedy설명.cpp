a42046eb7a5f8bed4bb20341abc705ccaf3f4fa6
JeongY00n <shimmumu@gmail.com>
2022-08-18 01:12:25 +0900

    Create greedy설명.cpp

---
+++ Greedy의 탑 Greedy 훈련/greedy설명.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+
+	// 현재 내가 마주한 상황에서 (최선 선택)<-"기준"
+	// 뒤를 생각 안하고 일단 지금것만 보고 선택
+
+	/*
+	10, 50, 100, 500 인 경우는 동전이서로 배수이기 때문에Greedy가 성립 됨.
+
+	만약 10, 70, 100 인 것처럼 동전이서로 배수가 아닌 경우Greedy가 성립 안됨.
+	*/
+
+	return 0;
+
+}
